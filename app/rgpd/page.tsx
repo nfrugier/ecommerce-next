4890fb6a157a5662350af845c10ecf01f6883059
unknown <nicolas.frugier2@gmail.com>
2025-04-28 21:37:59 +0200

    rectification pages de base cgv/rgpd

--- app/rgpd/page.tsx
+++ app/rgpd/page.tsx
@@ -1,9 +1,9 @@
 export default function Confidentialite() {
     return (
-      <div className="max-w-3xl mx-auto py-8 px-4">
+      <div className="max-w-7xl mx-auto py-8 px-4">
         <h1 className="text-3xl font-bold mb-4">Politique de confidentialité</h1>
         <p className="text-gray-700 leading-relaxed">
-          Conformément au RGPD, cette page vous permet d'expliquer comment les données personnelles de vos utilisateurs sont collectées, stockées et utilisées, et de préciser leurs droits (accès, rectification, suppression, etc.).
+          les conditions d'utilisation de données etc pour la RGPD vont ici
         </p>
       </div>
     );
